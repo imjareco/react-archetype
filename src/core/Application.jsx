2b9333b2a0d3bb625ff4698fdc8113c762d3be9d
Jhony Dev <jhony.reyescorredor@plexus.local>
2022-01-18 10:44:36 +0100

    Added i18next config for language translations

--- src/core/Application.jsx
+++ src/core/Application.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 import RoutesProvider from 'core/RoutesProvider';
-import translations from 'core/translations';
 import 'core/sanitize/reset.css';
 
 const queryClient = new QueryClient({
@@ -21,7 +20,7 @@ const queryClient = new QueryClient({
 const Application = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <I18nextProvider i18n={i18n(translations)} >
+      <I18nextProvider i18n={i18n} >
         <Router>
           <RoutesProvider />
         </Router>
---
+++ src/core/components/LanguageSelector.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Switch from '@mui/material/Switch';
+
+import { useTranslations, changeLanguage } from 'core/i18n';
+
+export const LanguageSelector = () =>  {
+    const { t } = useTranslations();
+    const [toggle, setToggle] = useState(false);
+
+    const handleChange = ({ target }) => {
+        const { checked } = target;
+        setToggle(checked);
+        changeLanguage(checked ? 'es' : 'en');
+    };
+
+    return(
+        <Box mt={'3px'}>
+            <Grid component="label" container alignItems="center" spacing={1}>
+                <Grid item>
+                    <Typography>{t('core.lang.english')}</Typography>
+                </Grid>
+                <Grid item>
+                    <Switch
+                        size='small'
+                        color="default"
+                        checked={toggle}
+                        onChange={handleChange}
+                        value="checked" 
+                    />
+                </Grid>
+                <Grid item>
+                <Typography>{t('core.lang.spanish')}</Typography>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+
+};
+
+LanguageSelector.defaultProps = {
+    options: [],
+};
+
+LanguageSelector.propTypes = {
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+    onChange: PropTypes.func.isRequired,
+};
+
